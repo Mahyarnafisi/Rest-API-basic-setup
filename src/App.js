5426017f49a69f81403e7d9d506722722da1828b
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-07-11 22:38:45 +0200

    improve the rest api fetching handling error and different situations

--- src/App.js
+++ src/App.js
@@ -5,41 +5,51 @@ import ListItem from "./Components/ListItem";
 function App() {
   const [fetchedData, setFetchedData] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const [error, setError] = useState("");
 
   async function loadingDataHandler() {
     setFetchedData([]);
-    setError(false);
+    setError("");
     setLoading(true);
-    const response = await fetch("https://swapi.dev/api/films/");
-    if (response.status !== 200) {
-      setError(true);
+    try {
+      const response = await fetch("https://swapi.dev/api/film/");
+      if (!response.ok) {
+        throw new Error("Error, something went wrong!");
+      }
+      const data = await response.json();
+      const modifiedData = data.results.map((item) => {
+        return {
+          id: (Math.random() * 100).toFixed(2),
+          episode: item.episode_id,
+          name: item.title,
+          producer: item.producer,
+        };
+      });
       setLoading(false);
-      return;
-    } else {
-      setError(false);
+      setFetchedData(modifiedData);
+    } catch (error) {
+      setError(error.message);
     }
-    const data = await response.json();
-
-    const modifiedData = data.results.map((item) => {
-      return {
-        id: (Math.random() * 100).toFixed(2),
-        episode: item.episode_id,
-        name: item.title,
-        producer: item.producer,
-      };
-    });
     setLoading(false);
-    setFetchedData(modifiedData);
+  }
+  let content = <p>no movie found!</p>;
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (loading) {
+    content = <p>Loading... </p>;
+  }
+
+  if (fetchedData.length > 0) {
+    content = <ListItem person={fetchedData} />;
   }
 
   return (
     <div className="App">
       <button onClick={loadingDataHandler}>Loading Data</button>
-      <ListItem person={fetchedData} />
-      {loading && <p>loading</p>}
-      {!loading && fetchedData.length === 0 && <p>No movie yet!</p>}
-      {error ? <p>Error</p> : ""}
+      {content}
     </div>
   );
 }
