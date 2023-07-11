7f422698020ddd7c1d71af585e2e30b9a8201ef0
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-07-11 17:01:26 +0200

    Initial new SORTED ARRAY

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,14 +1,23 @@
 import React from "react";
 
 const Item = (props) => {
+  const data = props.person.map((num) => {
+    return {
+      episode: num.episode,
+    };
+  });
+  const numList = data.map((episode) => {
+    return episode.episode;
+  });
+
   return (
     <div className="item">
       {props.person.map((item) => {
         return (
-          <div key={item.id}>
+          <div className="list-item" key={item.id}>
+            <h4>{item.episode}</h4>
             <h1>{item.name}</h1>
             <h2>{item.producer}</h2>
-            <h4>{item.episode}</h4>
           </div>
         );
       })}
